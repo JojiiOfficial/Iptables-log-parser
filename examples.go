8bfbb583dcbcbf55e73968306ae9fcb9cb8a39d6
Jojii <jojii@jojii.de>
2019-10-25 15:29:28 +0200

    added examples.go

---
+++ examples.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	//examle for reading the file line by line
+	err := ParseFileByLines("/var/log/Tripwire21", func(log *LogEntry) {
+		fmt.Println(log)
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	//examle for reading the file and use the array
+	logs, err := ParseFile("/var/log/Tripwire21")
+	if err != nil {
+		panic(err)
+	}
+	for _, log := range logs {
+		fmt.Println(log)
+	}
+}
+
